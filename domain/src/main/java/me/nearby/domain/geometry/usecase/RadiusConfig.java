0a9185b948bf7cff77b8604d4d3e92b75aa86fc9
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2024-10-06 13:38:30 +0700

    feat: define level for each radius

--- domain/src/main/java/me/nearby/domain/geometry/usecase/RadiusConfig.java
+++ domain/src/main/java/me/nearby/domain/geometry/usecase/RadiusConfig.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 import me.nearby.domain.geometry.usecase.exception.InvalidRadiusException;
 import me.nearby.objs.EnumEntity;
 
+/**
+ * Radius configuration: https://chatgpt.com/c/67022dc7-0e74-8011-836a-5669c4ec8f91
+ */
 @Getter
 @AllArgsConstructor
 public enum RadiusConfig implements EnumEntity<Integer> {
-    RADIUS_5KM(5, "5km", 10, 11),
-    RADIUS_15KM(15, "15km", 9, 10),
-    RADIUS_30KM(30, "30km", 8, 9),
-    RADIUS_50KM(50, "50km", 7, 8),
-    RADIUS_100KM(100, "100km", 6, 7),
+    RADIUS_5KM(5, "5km", 13, 13),
+    RADIUS_15KM(15, "15km", 12, 12),
+    RADIUS_30KM(30, "30km", 11, 11),
+    RADIUS_50KM(50, "50km", 10, 10),
+    RADIUS_100KM(100, "100km", 9, 9),
     ;
 
     private final Integer value;
